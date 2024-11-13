760159348689474248b9446fe28673958f7bf886
Will <=>
2024-11-13 14:04:25 -0800

    set up basic vue configuration. added tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0073c6', //blue
+        secondary: '#00274d', //dark blue
+        accent: '#00c77b', //green 
+        background: '#f8f8f8', //light grey
+        textPrimary: '#333', //dark grey
+      },
+    },
+  },
+  plugins: [],
+}
+
+
